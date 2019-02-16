3cbdebc5064599866a09e4518614908266dd2287
Luis <lfbtessarolli@gmail.com>
2019-02-16 17:39:04 -0200

    Ajuste documentacao API

--- ecommerce/fornecedor/urls.py
+++ ecommerce/fornecedor/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
+from rest_framework.documentation import include_docs_urls
 
 router = routers.DefaultRouter()
 router.register('fornecedor', views.FornecedorView)
@@ -10,4 +11,5 @@ router.register('agrofornecedor', views.AgroFornecedorView)
 urlpatterns = [
    path('', include(router.urls)),
    path('auth/', include('rest_auth.urls')),
+   path('docs/', include_docs_urls(title='API Fornedor')),
 ]
